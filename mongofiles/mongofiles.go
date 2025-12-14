3728399114f1852a7acf6ebf819eefeb44f4d24b
Felipe Gasper <felipe.gasper@mongodb.com>
2025-12-14 12:32:05 -0500

    prune

--- mongofiles/mongofiles.go
+++ mongofiles/mongofiles.go
@@ -21,8 +21,6 @@ import (
 	"github.com/mongodb/mongo-tools/common/options"
 	"github.com/mongodb/mongo-tools/common/util"
 	"github.com/pkg/errors"
-	"go.mongodb.org/mongo-driver/bson"
-	driverOptions "go.mongodb.org/mongo-driver/mongo/options"
 	"go.mongodb.org/mongo-driver/v2/bson"
 	"go.mongodb.org/mongo-driver/v2/mongo"
 	driverOptions "go.mongodb.org/mongo-driver/v2/mongo/options"
